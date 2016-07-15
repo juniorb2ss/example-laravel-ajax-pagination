45367d1889916bdb5e04dedfe0fbf3c6c3f4736b
Gabriel Pedro <gpedro831@gmail.com>
2016-07-15 12:31:51 -0400

    feat: add cleardb config

--- config/database.php
+++ config/database.php
@@ -1,5 +1,8 @@
 <?php
 
+// ClearDB Heroku
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
 return [
 
     /*
@@ -54,11 +57,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'localhost'),
+            'host' => $url['host'],
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => substr($url['path'], 1),
+            'username' => $url['user'],
+            'password' => $url['pass'],
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
